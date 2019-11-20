a6bce30578b920b45e9b1f3fcbe6c5f18a322e0c
ken.shen <352840792@qq.com>
2019-11-20 10:19:17 +0800

    1

--- src/views/demo/view/demo.vue
+++ src/views/demo/view/demo.vue
@@ -1,13 +1,12 @@
 <template>
   <div style="float:left">
     {{ msg }}
-      <div style="height:300px">123</div>
-    <tinymce ref="editor">
-    </tinymce>
-      <div style="height:300px">123</div>
-    <button  >清空内容</button>
-    <button  >禁用</button>
-    <button >启用</button>
+    <div style="height:300px">123</div>
+    <tinymce ref="editor"></tinymce>
+    <div style="height:300px">123</div>
+    <button>清空内容</button>
+    <button>禁用</button>
+    <button>启用</button>
   </div>
 </template>
 <script>
